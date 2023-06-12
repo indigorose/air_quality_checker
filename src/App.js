0ff10b289a847148633883da78436a489ba91831
indigorose <indigorose049@gmail.com>
2023-06-12 21:46:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Using the Async functionality allows for a better communication with external API sources.
+const getAirQuality = async (city) => {
+	try {
+		const response = await fetch(
+			`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_API_TOKEN}`
+		);
+		const data = await response.json();
+		console.log(data);
+	} catch {}
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <h1>Hello</h1>;
 }
 
 export default App;
