fd781426ce777381dd619574f002e89847874434
indigorose <indigorose049@gmail.com>
2023-06-16 19:30:16 +0100

    Conduct map api tests

--- src/CityLocationMap.js
+++ src/CityLocationMap.js
@@ -2,20 +2,48 @@ import React from 'react';
 
 function CityLocationMap({ location, city }) {
 	// Create a function that will display a static map of the location requested.
-	// const showLocation = async (location) => {
-	//     try {
-	//         const response = await fetch (`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${location[1]},${location[0]},12.15,0/600x400?access_token=${process.env.REACT_APP_MAP_API_TOKEN}`);
-	//     }
-	// }
+	const longitude = location[1].toFixed(4);
+	const latitude = location[0].toFixed(4);
+	// // eslint-disable-next-line no-unused-vars
+	// const [mapData, setMapData] = useState(null);
+	// // eslint-disable-next-line no-unused-vars
+	// const [error, setError] = useState(null);
+
+	// const LocationMap = async (longitude, latitude) => {
+	// 	try {
+	// 		const response = await fetch(
+	// 			`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${longitude},${latitude},12,0/600x400?access_token=${process.env.REACT_APP_MAP_API_TOKEN}`
+	// 		);
+	// 		const data = await response.json();
+	// 		console.log(data);
+	// 		if (response.ok && data.status === 'ok') {
+	// 			setMapData(data.data);
+	// 			setError(null);
+	// 		} else {
+	// 			setError(
+	// 				"Sorry, we couldn't find the city you were looking for. Try another location nearby or ensure your spelling is correct."
+	// 			);
+	// 			setMapData(null);
+	// 		}
+	// 	} catch (error) {
+	// 		// We need to catch errors as things happen outside of our control
+	// 		console.error('network error: ', error);
+	// 		setError('Sorry, something went wrong.');
+	// 		setMapData(null);
+	// 	}
+	// };
+
 	return (
 		<div className="card mb-4">
 			<div className="card-body">
 				<h4 className="card-title">{city}</h4>
 				<p>
-					Longitude: {location[1].toFixed(4)}, Latitude:{' '}
-					{location[0].toFixed(4)}
+					Longitude: {longitude}, Latitude: {latitude}
 				</p>
-				{/* <img src='{showLocation()}' alt="Map of city location" className="card" /> */}
+				{/* <img
+					src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${longitude},${latitude},12,0/600x400?access_token=${process.env.REACT_APP_MAP_API_TOKEN}`}
+					alt=""
+				/> */}
 			</div>
 		</div>
 	);
